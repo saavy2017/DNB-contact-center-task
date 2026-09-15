6209aeb9f2478c93b68c55c8b7e43313c241cf79
SR2017 <savitha.raju@gmail.com>
2026-09-15 10:23:02 +0530

    SupportCase - API included

--- source/CustomerCaseManagement/API.CaseManagement/Controllers/SupportCaseController.cs
+++ source/CustomerCaseManagement/API.CaseManagement/Controllers/SupportCaseController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Domain.CaseManagement.Entity;
+using Business.CaseManagement.Interface;
 
 namespace API.CaseManagement.Controllers
 {
@@ -7,10 +8,56 @@ namespace API.CaseManagement.Controllers
     [Route("api/v1/[controller]")]
     public class SupportCaseController : ControllerBase
     {
-        [HttpGet(Name = "GetSupportCases")]
-        public IEnumerable<SupportCase> Get()
+        private readonly ISupportCase _supportCaseService;
+        public SupportCaseController(ISupportCase supportCaseService)
         {
-            return null;
+            _supportCaseService = supportCaseService;
         }
+        [HttpGet(Name = "GetSupportCases")]
+        public async Task<IEnumerable<SupportCase>> Get()
+        {
+            return await _supportCaseService.GetAllAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetSupportCaseById")]
+        public async Task<ActionResult<SupportCase>> Get(int id)
+        {
+            var supportCase = await _supportCaseService.GetByIdAsync(id);
+            if (supportCase == null)
+            {
+                return NotFound();
+            }
+            return Ok(supportCase);
+        }
+
+        [HttpGet("search/{id}")]
+        public async Task<ActionResult<SupportCase>> Search(int id)
+        {
+            var supportCase = await _supportCaseService.Search(id);
+            if (supportCase == null)
+            {
+                return NotFound();
+            }
+            return Ok(supportCase);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SupportCase>> Post(SupportCase supportCase)
+        {
+            var createdSupportCase = await _supportCaseService.AddAsync(supportCase);
+            return CreatedAtAction(nameof(Get), new { id = createdSupportCase.Id }, createdSupportCase);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SupportCase>> Put(int id, SupportCase supportCase)
+        {
+            if (id != supportCase.Id)
+            {
+                return BadRequest();
+            }
+            var supportCaseUpdated = await _supportCaseService.UpdateAsync(supportCase);
+            return Ok(supportCaseUpdated);
+        }
+
     }
 }
--- source/CustomerCaseManagement/Business.CaseManagement/Service/SupportCaseService.cs
+++ source/CustomerCaseManagement/Business.CaseManagement/Service/SupportCaseService.cs
@@ -5,29 +5,34 @@ namespace Business.CaseManagement.Service
 {
     public class SupportCaseService : ISupportCase
     {
+        private readonly ISupportCase _supportCaseRepository;
+        public SupportCaseService(ISupportCase supportCaseRepository)
+        {
+            _supportCaseRepository = supportCaseRepository;
+        }
         public async Task<SupportCase> AddAsync(SupportCase supportCase)
         {
-            throw new NotImplementedException();
+            return await _supportCaseRepository.AddAsync(supportCase);
         }
 
         public async Task<SupportCase> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _supportCaseRepository.GetByIdAsync(id);   
         }
 
         public async Task<List<SupportCase>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _supportCaseRepository.GetAllAsync();
         }
 
         public async Task<SupportCase> UpdateAsync(SupportCase supportCase)
         {
-            throw new NotImplementedException();
+            return await _supportCaseRepository.UpdateAsync(supportCase);
         }
 
         public async Task<List<SupportCase>> Search(int id)
         {
-            throw new NotImplementedException();
+            return await _supportCaseRepository.Search(id);
         }
     }
 }
